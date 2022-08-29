21d8c25f15badb7f3f70b919a89115d4331deb2f
Andrii992 <andrey.kursom.na.tishiny@gmail.com>
2022-08-29 19:19:42 +0300

    Фінал 2го ДЗ

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+      ({ preview, original, description }) =>
+    `<a class='gallery__item' href='${original}'>
+    <img
+    class='gallery__image'
+    src='${preview}'
+    alt='${description}'/>
+    </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+gallery = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
