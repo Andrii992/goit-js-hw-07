0ed69d1307c17b9f35d251e52dffe49dcb42136d
Andrii992 <andrey.kursom.na.tishiny@gmail.com>
2022-08-29 18:18:34 +0300

    Перша частина ДЗ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+  .map(
+    (galleryItem) => `<div class='gallery__item'>
+    <a class='gallery__link' href='${galleryItem.original}'>
+    <img
+    class='gallery__image'
+    src='${galleryItem.preview}'
+    data-source='${galleryItem.original}'
+    alt='${galleryItem.description}'/>
+    </a>
+    </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+gallery.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  const isImgEl = e.target.classList.contains("gallery__image");
+
+  if (!isImgEl) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const originalImg = basicLightbox.create(
+    `<img src='${e.target.dataset.source}'>`
+  );
+
+  originalImg.show();
+
+  const modalIsOpen = document.querySelector(".basicLightbox");
+
+  if (modalIsOpen) {
+    console.log(modalIsOpen);
+
+    document.addEventListener("keydown", modalEscClose);
+
+    function modalEscClose(e) {
+      e.preventDefault();
+      console.log("esc");
+
+      if (e.code === "Escape") {
+        originalImg.close();
+        document.removeEventListener("keydown", modalEscClose);
+        return;
+      }
+    }
+  }
+}
